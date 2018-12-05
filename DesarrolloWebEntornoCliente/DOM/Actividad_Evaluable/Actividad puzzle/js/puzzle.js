bd5597a862ed673f607ff7deaca753b744d92474
Unknown <reginoboschlozano@gmail.com>
2018-12-05 15:44:32 +0100

    Subida para Actuliza contenido 05/12

--- DesarrolloWebEntornoCliente/DOM/Actividad_Evaluable/Actividad puzzle/js/puzzle.js
+++ DesarrolloWebEntornoCliente/DOM/Actividad_Evaluable/Actividad puzzle/js/puzzle.js
@@ -2,6 +2,7 @@
 let numPiezas = 0;
 let puntuacion = 0;
 let relacionAspecto = 0;
+let totalPiezas = 0;
 
 /**
  * Función Inicio Programa Pedimos un Número Válido al Usuario
@@ -110,12 +111,12 @@ function pieceNumberToRowsColumns(pieza, totalPiezas) {
 
     while (fila < raiz) {
         for (let i = 0; i < raiz; i++) {
-             array.push(columna + "," + i);
+            array.push(columna + "," + i);
         }
         fila++;
         columna++;
-    }    
-    return array[pieza - 1];
+    }
+    return array[pieza];
 }
 
 
@@ -123,57 +124,118 @@ function pieceNumberToRowsColumns(pieza, totalPiezas) {
  * @param {number} totalPiezas Le pasamos el total de piezas
  * @param {number} anchura Le pasamos la anchura del puzzle
  * @param {number} altura Le pasamos la altura del puzzle
- * @param {number} direccion Le pasamos la dirección de la imagen
-
+ * @param {number} direccion Le pasamos la dirección url de la imagen
  */
-function createPuzzleLayout(totalPiezas, anchura, altura, direccion)
-{
+function createPuzzleLayout(totalPiezas, anchura, altura, direccion) {
     let raiz = Math.sqrt(totalPiezas);
     // Obtener la referencia del elemento body
     var body = document.getElementsByTagName("body")[0];
-   
+
     // Crea un elemento <table> y un elemento <tbody>
-    var tabla   = document.createElement("table");
-    tabla.width =anchura;
+    var tabla = document.createElement("table");
 
 
     var tblBody = document.createElement("tbody");
-   
+
     // Crea las celdas
     for (var i = 0; i < raiz; i++) {
-      // Crea las hileras de la tabla
-      var hilera = document.createElement("tr");
-   
-      for (var j = 0; j < raiz; j++) {
-        // Crea un elemento <td> y un nodo de texto, haz que el nodo de
-        // texto sea el contenido de <td>, ubica el elemento <td> al final
-        // de la hilera de la tabla
-        var celda = document.createElement("td");
-        celda.setAttribute("id",i+","+j);
-        celda.height = altura;
-        var textoCelda = document.createTextNode(i+","+j);
-        
-        celda.style = "border: 3px solid black"; 
-        celda.style.backgroundImage = "url('./../cat.jpg')";
+        // Crea las hileras de la tabla
+        var hilera = document.createElement("tr");
 
-        celda.appendChild(textoCelda);
-        hilera.appendChild(celda);
-      }
-   
-      // agrega la hilera al final de la tabla (al final del elemento tblbody)
-      tblBody.appendChild(hilera);
+        for (var j = 0; j < raiz; j++) {
+            // Crea un elemento <td> y un nodo de texto, haz que el nodo de
+            // texto sea el contenido de <td>, ubica el elemento <td> al final
+            // de la hilera de la tabla
+            var celda = document.createElement("td");
+            celda.setAttribute("id", i + "," + j);
+            celda.height = altura / raiz;
+            celda.width = anchura / raiz;
+
+            var textoCelda = document.createTextNode(i + "," + j);
+
+            celda.style = "border: 3px solid black";
+            celda.style.backgroundImage = direccion;
+
+            celda.appendChild(textoCelda);
+            hilera.appendChild(celda);
+        }
+        // agrega la hilera al final de la tabla (al final del elemento tblbody)
+        tblBody.appendChild(hilera);
     }
-   
-    // posiciona el <tbody> debajo del elemento <table>
     tabla.appendChild(tblBody);
-    // appends <table> into <body>
     body.appendChild(tabla);
-    // modifica el atributo "border" de la tabla y lo fija a "2";
+}
 
-  //  tabla.setAttribute("border", "3px", "solid");
-    //tabla.setAttribute("style", "border: 3px solid ;");
-  }
+/** @description  Desplazamiento de las piezas del puzzle
+ * @param {number} numPieza Le pasamos el total de piezas
+ * @param {number} totalPiezas Le pasamos el total de piezas
+ * @param {number} anchura Le pasamos la anchura del puzzle
+ * @param {number} altura Le pasamos la altura del puzzle
+ * @return {number} desplazamiento Le devolveremos el desplazamiento de la pieza
+ **/
+function pieceToOffset(numPieza, totalPiezas, anchura, altura) {
 
+    let desplazamiento = {
+        "x": numPieza.x,
+        "y": numPieza.y
+    };
+    let alturaPieza = altura / Math.sqrt(totalPiezas);
+    let anchuraPieza = anchura / Math.sqrt(totalPiezas);
+
+    //document.getElementById(numPieza.x + "," + numPieza.y).style.backgroundPosition = "0px 0px";
+
+    /*for (let j = 0; j <= 1; j++) {
+        for (let i = 0; i <= 1; i++) {
+            numPieza.x = j;
+            numPieza.y = i;*/
+
+    desplazamiento.x = numPieza.y * anchuraPieza;
+    desplazamiento.y = numPieza.x * alturaPieza;
+    
+    return desplazamiento;
+    //document.getElementById(numPieza.x + "," + numPieza.y).style.backgroundPosition = "-"+desplazamiento.x + "px" + " " +"-"+desplazamiento.y + "px";
+    // }
+}
+
+/** @description  Devolverá un array con el desplazamiento (offset) 
+ * @param {number} totalPiezas Le pasamos el total de piezas
+ * @param {number} anchura Le pasamos la anchura del puzzle
+ * @param {number} altura Le pasamos la altura del puzzle
+ * @return {number} array con el desplazamiento (offset) 
+ **/
+function createReferenceSolution(totalPiezas, anchura, altura )
+{
+    let arrayPiezasPosc =[];
+    for (let j = 0; j < totalPiezas; j++) 
+    {
+        arrayPiezasPosc[j]= pieceNumberToRowsColumns(j, totalPiezas);
+        arrayPiezasPosc[j] =arrayPiezasPosc[j].split(",");
+    }
+    
+    
+    for (let i = 0; i < totalPiezas; i++) 
+    {
+        arrayPiezasPosc[i] = pieceToOffset( {"x":arrayPiezasPosc[i][0], "y":arrayPiezasPosc[i][1]}, totalPiezas,anchura, altura);
+    }
+    return arrayPiezasPosc;
+}
+/** @description  Devolverá un array con el desplazamiento (offset) 
+ * @param {array} arrayPosc Le pasamos el total de piezas
+ **/
+function drawContentPuzzle(arrayPosc)
+{
+    let arrayPiezasPosc =[];
+    for (let j = 0; j < arrayPosc.length; j++) 
+    {
+        arrayPiezasPosc[j]= pieceNumberToRowsColumns(j, arrayPosc.length);
+        arrayPiezasPosc[j] =arrayPiezasPosc[j].split(",");
+    }
+    for (let j = 0; j < arrayPosc.length; j++) 
+    {
+        document.getElementById(arrayPiezasPosc[j][0]+ "," +arrayPiezasPosc[j][1]).style.backgroundPosition = "-"+arrayPosc[j].x + "px" + " "+"-"+arrayPosc[j].y + "px";
+    }        
+}
+//LÓGICA DEL JUEGO
 
 //MAIN
 //Llamada función inicio del programa
@@ -183,4 +245,12 @@ function createPuzzleLayout(totalPiezas, anchura, altura, direccion)
 
 //PRUEBAS
 
-createPuzzleLayout(25, 800,100,2);
+createPuzzleLayout(4, 958, 1277, "url('./../cat.jpg')");
+
+/*console.log(pieceToOffset({
+    "x": 0,
+    "y": 1
+}, 4, 958, 1277));
+*/
+let array = createReferenceSolution(4, 958, 1277);
+drawContentPuzzle(array);
