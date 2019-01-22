205382438a328a1cdae9ce8a3931cb4ce26de8ab
Unknown <reginoboschlozano@gmail.com>
2019-01-22 19:50:52 +0100

    Actualizado !!

--- DesarrolloWebEntornoCliente/Angular/workspace/miPrimeraApp/src/app/app.component.ts
+++ DesarrolloWebEntornoCliente/Angular/workspace/miPrimeraApp/src/app/app.component.ts
@@ -7,4 +7,26 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'miPrimeraApp';
+  buttonText:string = "Save";
+  isDisabled:boolean = false;
+  name:string = "";
+  position:string = "";
+  saveEvent(){
+    if(this.buttonText === "Save"){
+      this.buttonText = "Edit";
+      this.isDisabled = true;
+    }
+    else{
+      this.buttonText = "Save";
+      this.isDisabled = false;
+    }
+  }
+
+  showInfomation(){
+    alert(this.name+" "+this.position);
+  }
+  reset(){
+    this.name=" ";
+    this.position=" ";
+  }
 }
